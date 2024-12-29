eb633ed88443bec64c545531155efacca4389d22
BomberFish <bomberfish@bomberfish.ca>
2024-12-28 20:59:32 -0500

    Initial Commit

---
+++ MGValueYoinkerTS/MGValueYoinkerTSApp.swift
@@ -0,0 +1,14 @@
+// bomberfish
+// MGValueYoinkerTSApp.swift – MGValueYoinkerTS
+// created on 2024-12-28
+
+import SwiftUI
+
+@main
+struct MGValueYoinkerTSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
